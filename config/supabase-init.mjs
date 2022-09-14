a9c44a679251c344c57ca3d44c7349c391b2f3c8
Aldimhr <dirikuin16@gmail.com>
2022-09-14 15:40:43 +0700

    fix: dotenv in esmodule

--- config/supabase-init.mjs
+++ config/supabase-init.mjs
@@ -1,5 +1,6 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
+import './loadenv.js';
 
 import { createClient } from '@supabase/supabase-js';
 
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import './config/loadenv.js';
 
 import { Telegraf } from 'telegraf';
 
