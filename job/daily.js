2a8a7edf31fd05dc53844411f6c74bdfb33a0834
Aldimhr <dirikuin16@gmail.com>
2022-09-17 11:51:30 +0700

    feat: test job daily workflow with push trigger

---
+++ job/daily.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+const { getUser, getConfig } = require('../database');
+const { getContents } = require('../utils/content');
+const errorHandler = require('../utils/errorHandler');
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+module.exports = async () => {
+  try {
+    const users = await getUser({ follow: true });
+
+    const _USER_FOUND = users.data.length;
+    if (_USER_FOUND) {
+      const { last_date_checked } = (await getConfig()).data[0];
+      let contents = await getContents(last_date_checked).then(
+        ({ filteredContents, randomContents }) => {
+          if (filteredContents.length) return filteredContents;
+          return randomContents;
+        }
+      );
+
+      users.data.forEach(async (user) => {
+        const chat_id = user.chat_id;
+        await bot.telegram.sendMessage(chat_id, contents.join('\n\n'), {
+          parse_mode: 'HTML',
+        });
+      });
+    }
+  } catch (err) {
+    errorHandler({ err, name: 'job/daily.js' });
+  }
+};
